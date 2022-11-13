3005f8c9d9ec8d59b72f5e688830043bc6cae574
Jacob Schulz <jaschulz0705@gmail.com>
2022-11-13 11:55:45 -0800

    enable keyvault setting for multiple keyvaults matching a condition

---
+++ keyvaults/enable-soft-delete.sh
@@ -0,0 +1,5 @@
+﻿
+
+
+# enables/disables soft-delete on keyvault
+az resource update --id /subscriptions/xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/<resourceGroupName>/providers/Microsoft.KeyVault/vaults/<keyVaultName> --set properties.enablePurgeProtection=true properties.enableSoftDelete=true
